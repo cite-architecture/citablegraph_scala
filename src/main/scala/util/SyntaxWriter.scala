eecd13e8ff2b5c6dfe371bf678ee253f73029dba
Christopher Blackwell <cwblackwell@gmail.com>
2017-02-02 20:31:39 -0500

    Minor change to test data; now exporting XML fragments for EZ inclusion in inventories.

--- src/main/scala/util/SyntaxWriter.scala
+++ src/main/scala/util/SyntaxWriter.scala
@@ -7,56 +7,235 @@ import java.io._
 import scala.io.Source
 import scala.collection.mutable.ArrayBuffer
 
-case class SyntaxWriter(st: SyntaxTokenizer) {
+case class SyntaxWriter(st: SyntaxTokenizer, outputDir: String = "target/tokenizations") {
+	val outputDirectory = outputDir
 
 	val exists = true
 
-	// all-purpose method for outputting an array of Strings to a file
-    def writeTextFile(a: Array[String], f: File) {
+	// for accessing records in a collectionArray line
+	private val COLLURN = 0
+	private val ANALYZEDTEXT = 1
+	private val TOKENTYPE = 2
+	private val DEFORMATION = 3
+	private val EDSTATUS = 4
+	private val DISCOURSE = 5
+	private val EXEMPLARURN = 6
+
+	// all-purpose method for outputting an array of Strings to the filesystem
+    def writeArrayToFile(a: Array[String], f: File) {
       val pw = new PrintWriter(f)
       for (l <- a) { pw.write(l); pw.write("\n") }
       pw.close
     }
 
+	// all-purpose method for outputting an array of Strings to the screen
+    def writeArrayViaHttp(a: Array[String]) {
+			throw new Exception("Writing files via HTTP not implemented yet.")
+    }
 
-	def writeOrcaFile(filename: String, orcaUrn: String, directory: String){
-			val v = st.makeOrcaCollection(orcaUrn)
-		   val d: java.io.File = checkDirectory(directory)
-		   val fn = s"""Orca_${filename}.tsv"""
-			val f = new File(s"${directory}/${fn}")
-			writeTextFile(v, f)
+	// Method for directing output
+	def directOutput(a: Array[String], fn: String){
+		writeArrayViaHttp(a)
 	}
 
-	def writeCts2Col(filename: String, directory: String) = {
+	// Method for directing output
+	def directOutput(a: Array[String], f: File){
+		writeArrayToFile(a, f)
+	}
+
+	def writeOrcaFile = {
+		val ourn = getOrcaUrn
+		val v = st.makeOrcaCollection(ourn)
+		val directory = createDirectory
+		val f = new File(s"${directory}/${getOrcaFilename}")
+		writeArrayToFile(v, f)
+	}
+
+	def writeCts2Col = {
 		val v = st.makeCts2ColText
-		val d: java.io.File = checkDirectory(directory)
-		val fn = s"""CTS-2col-${filename}.txt"""
-		val f = new File(s"${directory}/${fn}")
-		writeTextFile(v, f)
+		val directory = createDirectory
+		val f = new File(s"${directory}/${getCTSFilename}")
+		writeArrayToFile(v, f)
 	}
 
 	// writes collectionArray to a file
-	def writeCollection(filename: String, directory: String) {
-		val d: java.io.File = checkDirectory(directory)
-		val f = new File(s"${directory}/TokenCollection-${filename}.tsv")
+	def writeCollection {
+		val directory = createDirectory
+		val f = new File(s"${directory}/${getCollectionFilename}")
 		var tempArray =  scala.collection.mutable.ArrayBuffer[String]()
 		tempArray += "Urn\tAnalyzedText\tTokenType\tTextDeformation\tEditorialStatus\tDiscourseLevel\tExemplarUrn\tSequence"
 		for (i <- 0 until st.collectionArray.size){
 			tempArray += s"${st.collectionArray(i)}\t${i}"
 		}
-		writeTextFile(tempArray.toArray, f)
+		writeArrayToFile(tempArray.toArray, f)
 	}
 
+	def writeCollectionInventory = {
+			val x = s"""
+<!-- Syntax tokens from ${getCtsUrn} -->
+<citeCollection canonicalId="Urn" label="TextDeformation" urn="${getCollectionUrn}">
+  <namespaceMapping abbr="***ADD NAMESPACE ABBR***" uri="***ADD NS URI***"/>
+
+  <dc:title xmlns="http://purl.org/dc/elements/1.1/">Syntax tokenization of ${getCtsUrn}</dc:title>
+  <description xmlns="http://purl.org/dc/elements/1.1/">Syntax tokenization with categorized tokens, analyzed from ${getCtsUrn}</description>
+  <rights xmlns="http://purl.org/dc/elements/1.1/"> All data in this collection are available
+      under the terms of the Creative Commons Attribution-Non-Commercial 3.0 Unported License,
+      http://creativecommons.org/licenses/by-nc/3.0/deed.en_US</rights>
+
+  <orderedBy property="Sequence"/>
+  <source type="file" value="${getCollectionFilename}"/>
+
+  <citeProperty name="Urn" label="The URN for this token" type="cite2urn"></citeProperty>
+  <citeProperty name="AnalyzedText" label="CTS Urn with string alignment" type="ctsurn"></citeProperty>
+  <citeProperty name="TokenType" label="Token type" type="cite2urn"></citeProperty>
+  <citeProperty name="TextDeformation" label="Presentational String" type="string"></citeProperty>
+  <citeProperty name="EditorialStatus" label="Editorial Status" type="cite2urn"/>
+  <citeProperty name="DiscourseLevel" label="Discourse Level" type="cite2urn"/>
+  <citeProperty name="Sequence" label="Sequence" type="number"></citeProperty>
+  <citeProperty name="ExemplarUrn" label="CTS Urn for derived analytical exemplar" type="ctsurn"></citeProperty>
+
+</citeCollection>
+			"""
+			val v = Array(x)
+			val directory = createDirectory
+			val f = new File(s"${directory}/FOR-COLLECTION-INVENTORY_Tokens.xml")
+			writeArrayToFile(v, f)
+
+	}
+
+	def writeOrcaCollectionInventory = {
+			val x = s"""
+<!-- ORCA alignment of Syntax tokens (${getCollectionUrn}) with ${getCtsUrn} -->
+<citeCollection canonicalId="ORCA_URN" label="TextDeformation" urn="${getOrcaUrn}">
+		<namespaceMapping abbr="***ADD NAMESPACE ABBR***" uri="***ADD NS URI***"/>
+		<extendedBy extension="cite:ORCA"/>
+
+		<dc:title xmlns="http://purl.org/dc/elements/1.1/">ORCA Alignment of  ${getCollectionUrn} with ${getCtsUrn} </dc:title>
+
+		<description xmlns="http://purl.org/dc/elements/1.1/">ORCA Alignment of Syntax tokens ${getCollectionUrn} with ${getCtsUrn}</description>
+		<rights xmlns="http://purl.org/dc/elements/1.1/"> All data in this collection are available
+				under the terms of the Creative Commons Attribution-Non-Commercial 3.0 Unported License,
+				http://creativecommons.org/licenses/by-nc/3.0/deed.en_US</rights>
+
+		<orderedBy property="Sequence"/>
+		<source type="file" value="${getOrcaFilename}"/>
+
+		<citeProperty name="ORCA_URN" label="The URN for this token" type="cite2urn"></citeProperty>
+		<citeProperty name="AnalyzedText" label="CTS Urn with string alignment" type="ctsurn"></citeProperty>
+		<citeProperty name="Analysis" label="Token Type" type="cite2urn"></citeProperty>
+		<citeProperty name="TextDeformation" label="Presentational String" type="string"></citeProperty>
+		<citeProperty name="Sequence" label="Sequence" type="number"></citeProperty>
+		<citeProperty name="ExemplarUrn" label="CTS Urn for derived analytical exemplar" type="ctsurn"></citeProperty>
+
+</citeCollection>
+			"""
+			val v = Array(x)
+			val directory = createDirectory
+			val f = new File(s"${directory}/FOR-COLLECTION-INVENTORY_orca.xml")
+			writeArrayToFile(v, f)
+
+	}
+
+	def writeCtsInventory = {
+			val x = s"""
+<inventoryFragments>
+
+	<! -- For TextInventory: insert in Edition element for '${getCtsUrn}' -->
+	<exemplar urn="${getCtsExemplarUrn}">
+		 <label xml:lang="eng">Syntax Tokens</label>
+		 <description xml:lang="eng">Syntactically Significant Tokens</description>
+		 <online/>
+	</exemplar>
+
+	<! -- For CitationConfig -->
+	<online urn="${getCtsExemplarUrn}" type="2col" docname="${getCTSFilename}" nodeformat="text">
+     <citationScheme>
+         <!-- You need to add this yourself! -->
+     </citationScheme>
+	</online>
+
+
+</inventoryFragments>
+			"""
+			val v = Array(x)
+			val directory = createDirectory
+			val f = new File(s"${directory}/FOR-CTS-INVENTORY-AND-CITATION-CONFIG.xml")
+			writeArrayToFile(v, f)
+
+	}
+
+
 	/* PRIVATE METHODS */
+
+	private def getOrcaFilename = {
+		val fn = s"ORCA-${getBaseFileName}.txt"
+		fn
+	}
+
+	private def getCollectionFilename = {
+		val fn = s"TokenCollection-${getBaseFileName}.tsv"
+		fn
+	}
+
+	private def getCTSFilename = {
+		val fn = s"CTS2Col-${getBaseFileName}.txt"
+		fn
+	}
 
 	private def checkDirectory(d: String): java.io.File = {
 		val dir: File = new File(d)
 		if ( dir.exists() != true ){
-			dir.mkdir()
+			dir.mkdirs()
 			dir
 		} else {
 			dir
 		}
+	}
+
+	private def createDirectory: String = {
+		val s = getBaseFileName
+		val p = s"${outputDirectory}/${s}"
+		val dir = checkDirectory(p)
+		p
+	}
+
+	private def getBaseFileName: String = {
+		  val us = st.collectionArray(0).split("\t")(ANALYZEDTEXT)
+			val urn = CtsUrn(us).dropPassage
+			val basefilename = urn.toString.replaceAll("urn:cts:","").replaceAll(":","_").replaceAll("\\.","-").dropRight(1)
+			basefilename
+	}
+
+	private def getCtsUrn: String = {
+		  val us = st.collectionArray(0).split("\t")(ANALYZEDTEXT)
+			val urn = CtsUrn(us).dropPassage
+			val urnString = urn.toString
+			urnString
+	}
+
+
+	private def getCtsExemplarUrn: String = {
+		  val us = st.collectionArray(0).split("\t")(EXEMPLARURN)
+			val urn = CtsUrn(us).dropPassage
+			val urnString = urn.toString
+			urnString
+	}
+
+
+	private def getOrcaUrn: String = {
+		  val us = st.collectionArray(0).split("\t")(COLLURN)
+			val urn = Cite2Urn(us)
+			val orcaurn = s"urn:cite2:${urn.namespace}:${urn.collection}_ORCA.${urn.version}:"
+
+		  orcaurn
+	}
+
+	private def getCollectionUrn: String = {
+		  val us = st.collectionArray(0).split("\t")(COLLURN)
+			val urn = Cite2Urn(us)
+			val collurn = s"urn:cite2:${urn.namespace}:${urn.collection}.${urn.version}:"
+
+		  collurn
 	}
 
 
--- src/test/scala/cgs/SyntaxWriterSpec.scala
+++ src/test/scala/cgs/SyntaxWriterSpec.scala
@@ -21,8 +21,21 @@ class SyntaxWriterSpec extends FlatSpec with PrivateMethodTester {
 			assert(sw.exists)
 	}
 
+	it should "have a private mathod for constructing a base filename from the CTS URN" in {
+ 			val f = "/Users/cblackwell/Dropbox/CITE/scala/citableGraph/src/test/resources/pericles-short-tab.txt"
+			val cus = "urn:cite2:fufolio:syntaxToken.v1:0007_0012_"
+			val outDir = "testOutput"
+			val st = SyntaxTokenizer("filesystem",f,cus)
+			val sw = SyntaxWriter(st)
+
+			val getBaseFileName = PrivateMethod[String]('getBaseFileName)
+			val aa = sw invokePrivate getBaseFileName()
+			assert (aa == "greekLit_tlg0007-tlg012-ziegler")
+	}
+
 	it should "have a private method to check for a specified output directory" in pending
 
+/*
 	it should "be able to process a non-xml text to a collectionArray to a file" in {
  			val inputf = "src/test/resources/pericles-short-tab.txt"
 			val cus = "urn:cite2:fufolio:syntaxToken.v1:0007_0012_"
@@ -32,13 +45,57 @@ class SyntaxWriterSpec extends FlatSpec with PrivateMethodTester {
 			val sw = SyntaxWriter(st)
 			sw.writeCollection(outputfile, outputdir)
 	}
+	*/
 
-	it should "be able to write a full token collection" in pending
-	it should "be able to write a collection-inventory fragment for the token collection" in pending
-	it should "be able to write an ORCA collection" in pending
-	it should "be able to write a collection-inventory fragment for the ORCA collection" in pending
-	it should "be able to write a 2-column CTS Analytical Exemplar" in pending
-	it should "be able to write a TextInventory and CitationConfig fragment for the Analytical exemplar" in pending
+	it should "be able to write a full token collection" in {
+ 			val tabfile = "src/test/resources/pericles-short-tab.txt"
+			val collectionurn = "urn:cite2:fufolio:short.v1:"
+			val st = SyntaxTokenizer("filesystem",tabfile,collectionurn)
+			val sw = SyntaxWriter(st)
+			sw.writeCollection
+	}
+
+
+	it should "be able to write a collection-inventory fragment for the token collection" in {
+ 			val tabfile = "src/test/resources/pericles-short-tab.txt"
+			val collectionurn = "urn:cite2:fufolio:short.v1:"
+			val st = SyntaxTokenizer("filesystem",tabfile,collectionurn)
+			val sw = SyntaxWriter(st)
+			sw.writeCollectionInventory
+	}
+
+	it should "be able to write an ORCA collection" in {
+ 			val tabfile = "src/test/resources/pericles-short-tab.txt"
+			val collectionurn = "urn:cite2:fufolio:short.v1:"
+			val st = SyntaxTokenizer("filesystem",tabfile,collectionurn)
+			val sw = SyntaxWriter(st)
+			sw.writeOrcaFile
+	}
+
+	it should "be able to write a collection-inventory fragment for the ORCA collection" in {
+ 			val tabfile = "src/test/resources/pericles-short-tab.txt"
+			val collectionurn = "urn:cite2:fufolio:short.v1:"
+			val st = SyntaxTokenizer("filesystem",tabfile,collectionurn)
+			val sw = SyntaxWriter(st)
+			sw.writeOrcaCollectionInventory
+	}
+
+	it should "be able to write a 2-column CTS Analytical Exemplar" in {
+ 			val tabfile = "src/test/resources/pericles-short-tab.txt"
+			val collectionurn = "urn:cite2:fufolio:short.v1:"
+			val st = SyntaxTokenizer("filesystem",tabfile,collectionurn)
+			val sw = SyntaxWriter(st)
+			sw.writeCts2Col
+	}
+
+	it should "be able to write a TextInventory and CitationConfig fragment for the Analytical exemplar" in {
+ 			val tabfile = "src/test/resources/pericles-short-tab.txt"
+			val collectionurn = "urn:cite2:fufolio:short.v1:"
+			val st = SyntaxTokenizer("filesystem",tabfile,collectionurn)
+			val sw = SyntaxWriter(st)
+			sw.writeCtsInventory
+	}
+	
 	it should "be able to write an index file for exemplifies/exemplifiedBy" in pending
 	it should "be able to write an index file for analyzes/analyzedBy" in pending
 	it should "be able to write an index file for hasAnalysis/isAnalysisFor" in pending
